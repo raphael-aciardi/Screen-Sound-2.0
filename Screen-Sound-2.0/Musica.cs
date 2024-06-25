b07c3da556ef7435fa5e4f3511ce55fd22c4d81c
raphael-aciardi <996193248ph@gmail.com>
2024-06-25 19:39:24 -0300

    teste

---
+++ Screen-Sound-2.0/Musica.cs
@@ -0,0 +1,22 @@
+﻿class Musica
+{
+    public string nome;
+    public string artista;
+    public int duracao;
+    public bool disponivel;
+
+    public void ExibirFichaTecnica()
+    {
+        Console.WriteLine($"Nome: {nome}");
+        Console.WriteLine($"Artista: {artista}");
+        Console.WriteLine($"Duração: {duracao}");
+        if (disponivel)
+        {
+            Console.WriteLine("Disponível no plano.");
+        }
+        else
+        {
+            Console.WriteLine("Adquira o plano Plus+");
+        }
+    }
+}
---
+++ Screen-Sound-2.0/Program.cs
@@ -0,0 +1,14 @@
+﻿Musica musica1 = new Musica();
+musica1.nome = "Roxane";
+musica1.artista = "The Police";
+musica1.duracao = 273;
+musica1.disponivel = true;
+
+Musica musica2 = new Musica();
+musica2.nome = "Vertigo";
+musica2.artista = "U2";
+musica2.duracao = 367;
+musica2.disponivel = false;
+
+musica1.ExibirFichaTecnica();
+musica2.ExibirFichaTecnica();
